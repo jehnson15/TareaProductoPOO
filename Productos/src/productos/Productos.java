b09e9eed8186ca16a5a5767261555c1dbfa0d271
Jehnson Jesus Carcamo Rivera <jehnson.carcamo@unah.hn>
2024-07-19 23:50:48 -0600

    Add files via upload

---
+++ Productos/src/productos/Productos.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package productos;
+public class Productos {
+
+    public static void main(String[] args) {
+         
+        CajaFacturacion caja = new CajaFacturacion();
+        caja.setVisible(true);
+      }
+    }
+
